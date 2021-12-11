14ae6f19cd4f3171ffd9b539a7414084ec5e54d8
Masahiro Kaga <masahiro41802891@gmail.com>
2021-12-11 13:33:15 -0800

    230_How to work code

--- redux-demo.js
+++ redux-demo.js
@@ -1,8 +1,15 @@
 const redux = require("redux");
 
-const counterReducer = (state = { counter : 0} ,action) => {
-    return{
-        counter:state.counter + 1
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "increment") {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1
+        }
     }
 };
 
@@ -10,12 +17,13 @@ const store = redux.createStore(counterReducer);
 
 // console.log(store.getState());
 
-const counterSubscriber = () =>{
+const counterSubscriber = () => {
     const latestState = store.getState();
     console.log(latestState);
 }
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type:"increment"});
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
 
